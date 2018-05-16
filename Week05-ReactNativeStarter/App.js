45a3f11a87025c613bd91ecf20cde0e01d5ef7bb
daryk on laptop <daryk.kohler@gmail.com>
2018-05-16 03:40:09 -0700

    Finished ReactNativeBasics assignment

---
+++ Week05-ReactNativeStarter/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}/>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
